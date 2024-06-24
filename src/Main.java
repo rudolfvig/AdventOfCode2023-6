8bc3c809e709685a6ad6b0ee6e1e79a6fbf8253b
vigRudolf <vrudi68@gmail.com>
2024-06-24 11:53:20 +0200

    understanding the exercise

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int length = 100;
+        HashMap<Integer, Integer> racePossibilities = new HashMap<>();
+        for (int i = 0; i <= length; i++) {
+            System.out.println("distance traveled in powered " + i + ": " + (length - i) * i);
+            racePossibilities.put(i, length - i);
+        }
+        
+
     }
 }
